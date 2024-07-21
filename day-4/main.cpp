3dd6574ac3139225af5ccd20a3dca304de794852
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-21 23:07:45 +0530

    day4

--- day-4/main.cpp
+++ day-4/main.cpp
@@ -48,3 +48,96 @@ public:
 
     }
 };
+
+
+// Question -6
+// 876. Middle of the Linked List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next!= nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+
+// Question - 7
+// 141. Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+        if(fast == slow){
+            return true;
+        }
+        }
+        return false;
+    }
+};
+
+// quetion - 8
+// 83. Remove Duplicates from Sorted List
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return nullptr;
+
+        ListNode* cur = head;
+
+        while (cur->next != nullptr){
+            if (cur->val == cur->next->val){
+                ListNode* temp = cur->next;
+                cur->next = temp ->next;
+                delete temp;
+            }
+            else{
+                cur = cur->next;
+            }
+
+        }
+        return head;
+    }
+};
