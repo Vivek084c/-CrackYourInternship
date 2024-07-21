3b36c1ef693ea3f9f26f30e89806bf75fcb0e5b2
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-21 16:46:34 +0530

    day -4

--- day-4/main.cpp
+++ day-4/main.cpp
@@ -19,3 +19,32 @@ public:
      return {};
     }
 };
+
+
+//question - 5
+// 88. Merge Sorted Array
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int insertPointer = m + n -1;
+
+    
+    while (m>0 && n>0){
+        if (nums1[m-1]>nums2[n-1]){
+            nums1[insertPointer--] = nums1[m-1];
+            --m;
+        }
+        else{
+            nums1[insertPointer--] = nums2[n-1];
+            --n;
+        }
+    }
+    // filling nums1
+    while (n>0){
+        nums1[insertPointer--] = nums2[n-1];
+        --n;
+    }
+
+    }
+};
