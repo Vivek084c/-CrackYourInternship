5b10c627c36cc7fd372a65b1e9f6d8e9e01f5595
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-22 22:34:46 +0530

    day-5

--- day-5/main.cpp
+++ day-5/main.cpp
@@ -90,3 +90,60 @@ public:
         return prev;
     }
 };
+
+
+// 234. Palindrome Linked List
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next){
+            return true;
+        }
+
+        // finding midle of linked list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        //reversing the second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow->next;
+        while (curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        ListNode* revNode = prev;
+
+        //comparing first and second half
+        ListNode* p1 = head;
+        ListNode* p2 = revNode;
+        bool ispalin = true;
+        while (p2){
+            if (p1->val != p2->val){
+                ispalin = false;
+                break;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return ispalin;
+
+    }
+};
